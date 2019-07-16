10c3ce08be90bc6a3c71f62b963678e736799e1e
Daniil Bondar <24-test@bk.ru>
2019-07-16 10:13:01 +0300

    R1.1

--- api.php
+++ api.php
@@ -6,16 +6,21 @@ $error = "";
 $pid = "";
 
 if (isset($_GET['act'])) {
-    if($_GET['act'] == "auth"){
+    if ($_GET['act'] == "auth") {
+        $login = $_GET['login'];
+        $passwd = $_GET['password'];
+        $usr = $db->select('users', "username = '" . $login . "' AND password = '" . $passwd . "'");
+        if ($usr['password'] == $passwd) {
+            $result['answer'] = "OK";
+            $group = $db->select('groups', "id = '" . $usr['group_id'] . "'");
+            $result['group'] = $group['name'];
+        } else $result['answer'] = "ERR";
+        echo json_encode($result);
+    } else if ($_GET['act'] == "request") {
         echo 'INDEV';
-    }
-    else if($_GET['act'] == "request"){
+    } else if ($_GET['act'] == "notify") {
         echo 'INDEV';
-    }
-    else if($_GET['act'] == "notify"){
-        echo 'INDEV';
-    }
-    else if($_GET['act'] == "getlog"){
+    } else if ($_GET['act'] == "getlog") {
         echo 'INDEV';
     }
 } else die('HACKING_ATTEMPT');
