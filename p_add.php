426eb3a9f75eeb42f3e55d752b3617cb93603df0
Daniil Bondar <24-test@bk.ru>
2019-09-06 18:03:26 +0300

    R1.1

--- p_add.php
+++ p_add.php
@@ -16,17 +16,23 @@ if ($user->admin < 2) {
 }
 
 if (isset($_POST['submit'])) {
-    $data['f'] = "'".$_POST['f']."'";
-    $data['i'] = "'".$_POST['i']."'";
-    $data['o'] = "'".$_POST['o']."'";
-    $data['email'] = "'".$_POST['email']."'";
-    $data['username'] = "'".$_POST['username']."'";
-    $data['group_id'] = "'".$_POST['group_id']."'";
-    $password = mt_rand(1000, 9999);
-    $data['password'] = "'".md5($password)."'";
-    $data['join_date'] = "'" . date("Y-m-d H:i:s", time()) . "'";
-    $itog = $db->insert($data, 'users');
-    $msg = "Регистрация произведена успешно. <strong>ПАРОЛЬ УЧАСТНИКА - ".$password.'</strong>';
+    foreach($_POST['fio'] as $key=>$item) {
+        if ($_POST['fio'][$key] != "") {
+            $fio = $_POST['fio'][$key];
+            $fio = explode(" ", $fio);
+            $data['f'] = "'" . $fio[0] . "'";
+            $data['i'] = "'" . $fio[1] . "'";
+            $data['o'] = "'" . $fio[2] . "'";
+            $data['username'] = "'" . $_POST['username'][$key] . "'";
+            $data['group_id'] = "'" . $_POST['group_id'] . "'";
+            $password = mt_rand(1000, 9999);
+            $data['password'] = "'" . md5($password) . "'";
+            $data['join_date'] = "'" . date("Y-m-d H:i:s", time()) . "'";
+            $itog = $db->insert($data, 'users');
+            echo $_POST['username'][$key] . '; ' . $_POST['fio'][$key] . '; ' . $password . '<br>';
+        }
+    }
+    echo '<strong>Операция завершена успешно.</strong>';
 }
 
 require_once 'includes/header.inc.php';
@@ -41,12 +47,8 @@ require_once 'includes/header.inc.php';
     <h1><?php echo $_SESSION['grand']['name']; ?></h1>
     <?php if(isset($msg)) echo "<h3>".$msg."</h3>"; ?>
     <form class="md-form border border-light p-5" action="p_add.php" method="post">
-        <p class="h4 mb-4 text-center">Регистрация участника</p>
-        <input type="text" id="textInput" name="f" class="form-control mb-4" placeholder="Фамилия">
-        <input type="text" id="textInput" name="i" class="form-control mb-4" placeholder="Имя">
-        <input type="text" id="textInput" name="o" class="form-control mb-4" placeholder="Отчество">
-        <input type="text" id="textInput" name="email" class="form-control mb-4" placeholder="E-mail">
-        <input type="text" id="textInput" name="username" class="form-control mb-4 fixed-15em" placeholder="ID системы ЕИС">
+        <p class="h4 mb-4 text-center">Регистрация участников </p>
+
         <select class="browser-default custom-select mb-4" id="select" name="group_id">
             <?php
             $sections = $db->select_fs('groups', "id != '0'");
@@ -56,6 +58,25 @@ require_once 'includes/header.inc.php';
             }
             ?>
         </select>
+
+        <table>
+            <thead>
+            <tr>
+                <td>ФИО</td>
+                <td>ИД ЕИС</td>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            for($i = 1; $i <= 30; $i = $i + 1) {
+                echo '<tr>';
+                echo '<td><input type="text" id="textInput" name="fio[]" class="form-control mb-4" placeholder="ФИО участника"></td>';
+                echo '<td><input type="text" id="textInput" name="username[]" class="form-control mb-4 fixed-15em" placeholder="ID системы ЕИС"></td>';
+                echo '</tr>';
+            }
+        ?>
+            </tbody>
+        </table>
         <button class="btn btn-info btn-block" type="submit" name="submit">Зарегистрировать</button>
     </form>
 </body>
