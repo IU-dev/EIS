34d0d76d0532a8d7fff201450cb48ae41f26faf2
Daniil Bondar <24-test@bk.rugit commit -a -m Initial>
2019-11-01 22:56:49 +0400

    R1.3

---
+++ info.php
@@ -0,0 +1,191 @@
+<?php
+
+require_once 'includes/global.inc.php';
+$page = "myclass.php";
+
+if (!isset($_SESSION['logged_in'])) {
+    header("Location: login.php");
+}
+
+$user = unserialize($_SESSION['user']);
+
+if (!isset($_GET['uid'])) {
+    die('Отсутствует UID.');
+} else {
+    $uid = $_GET['uid'];
+    $usr = $userTools->get($uid);
+}
+
+if ($user->admin < 1) {
+    header("Location: access_denied.php");
+}
+
+require_once 'includes/header.inc.php';
+?>
+<html>
+<head>
+    <title>Список участников | <?php echo $pname; ?></title>
+</head>
+<body><br>
+<div class="container">
+    <div class="row align-items-center">
+        <div class="col-3 justify-content-center">
+            <img src="placeholder.png"
+                 alt="avatar mx-auto white" class="rounded-circle img-fluid">
+        </div>
+        <div class="col-9">
+            <h1><?php echo $usr->f . ' ' . $usr->i . ' ' . $usr->o; ?></h1>
+            <h3>Состоит в группе
+                <?php $gr = $db->select('groups', "id = '" . $usr->group_id . "'");
+                echo $gr['name']; ?>
+            </h3>
+            <table class="table table-bordered table-hover table-striped table-sm">
+                <thead>
+                <tr>
+                    <th scope="col">Параметр</th>
+                    <th scope="col">Значение</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Дата рождения</td>
+                    <td>01.01.2001</td>
+                </tr>
+                <tr>
+                    <td>Телефон</td>
+                    <td>+7 (912) 000 00-00</td>
+                </tr>
+                <tr>
+                    <td>Электронная почта</td>
+                    <td>24-test@bk.ru</td>
+                </tr>
+                <tr>
+                    <td>Личное дело</td>
+                    <td>И-000</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <ul class="nav nav-tabs md-tabs primary-color" id="myTabMD" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link active" id="home-tab-md" data-toggle="tab" href="#writes" role="tab"
+                   aria-controls="home-md"
+                   aria-selected="true">Записи</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="profile-tab-md" data-toggle="tab" href="#accs" role="tab"
+                   aria-controls="profile-md"
+                   aria-selected="false">Аккаунты</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="contact-tab-md" data-toggle="tab" href="#contact-md" role="tab"
+                   aria-controls="contact-md"
+                   aria-selected="false">Мониторинги</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="contact-tab-md" data-toggle="tab" href="#contact-md" role="tab"
+                   aria-controls="contact-md"
+                   aria-selected="false">История действий</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="contact-tab-md" data-toggle="tab" href="#contact-md" role="tab"
+                   aria-controls="contact-md"
+                   aria-selected="false">Личный рейтинг</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="contact-tab-md" data-toggle="tab" href="#contact-md" role="tab"
+                   aria-controls="contact-md"
+                   aria-selected="false">Персональные данные</a>
+            </li>
+        </ul>
+        <div class="tab-content card pt-5" id="myTabContentMD">
+            <div class="tab-pane fade show active" id="writes" role="tabpanel" aria-labelledby="profile-tab-md">
+                <table id="writes2" class="table table-bordered table-hover table-striped table-sm table-responsive">
+                    <thead>
+                    <tr>
+                        <th scope="col">NOT-</th>
+                        <th scope="col">Автор</th>
+                        <th scope="col">Дата</th>
+                        <th scope="col">Содержание</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>611</td>
+                        <td>Бондарь Даниил Олегович (ЕИС-1)</td>
+                        <td>01.11.2019 22:37:00</td>
+                        <td>По результатам профориентационного тестирования имеет склонность к техническим дисциплинам
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>577</td>
+                        <td>Грудцина Ольга Михайловна (ЕИС-2)</td>
+                        <td>23.10.2019 14:25:31</td>
+                        <td>Приглашен на встречу обучающихся с Начальником Управления образования Администрации города
+                            Ижевска.
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="tab-pane fade" id="accs" role="tabpanel" aria-labelledby="profile-tab-md">
+                <table id="accs2" class="table table-bordered table-hover table-striped table-sm table-responsive">
+                    <thead>
+                    <tr>
+                        <th scope="col">ACC-</th>
+                        <th scope="col">Система</th>
+                        <th scope="col">Логин</th>
+                        <th scope="col">Действие</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    $accs = $db->select_fs('accounts', "user_eis = '" . $usr->username . "'");
+                    foreach ($accs as $acc) {
+                        echo '<tr>';
+                        echo '<td>' . $acc['id'] . '</td>';
+                        $srv = $db->select('services', "id = '" . $acc['service_id'] . "'");
+                        echo '<td>' . $srv['name'] . '</td>';
+                        echo '<td>' . $acc['login'] . '</td>';
+                        echo '<td>Нет доступных действий.</td>';
+                        echo '</tr>';
+                    }
+                    ?>
+                    </tbody>
+                </table>
+            </div>
+            <div class="tab-pane fade" id="contact-md" role="tabpanel" aria-labelledby="contact-tab-md">
+                <p>Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo
+                    retro fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft
+                    beer,
+                    iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony.
+                    Leggings gentrify squid 8-bit cred pitchfork. Williamsburg banh mi whatever gluten-free, carles
+                    pitchfork biodiesel fixie etsy retro mlkshk vice blog. Scenester cred you probably haven't heard of
+                    them, vinyl craft beer blog stumptown. Pitchfork sustainable tofu synth chambray yr.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require_once 'includes/footer.inc.php'; ?>
+<script>
+
+    $(document).ready(function () {
+        $('#writes2').DataTable({
+            "order": [[0, "desc"]]
+        });
+        $('.dataTables_length').addClass('bs-select');
+    });
+
+    $(document).ready(function () {
+        $('#accs2').DataTable({
+            "order": [[0, "asc"]]
+        });
+        $('.dataTables_length').addClass('bs-select');
+    });
+
+</script>
+</body>
+</html>
