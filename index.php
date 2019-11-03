f79f9fdd9e6f18cffba5864af88bda93d93448f3
Daniil Bondar <24-test@bk.ru>
2019-11-04 00:02:06 +0400

    R1.3 U3

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ require_once 'includes/header.inc.php';
     <h3>МБОУ "ИТ-лицей №24"</h3><br>
     <div class="alert alert-info" role="alert">
         <h3>Добро пожаловать!</h3>
-        <em>Текущая версия системы: R1.2</em><br>
+        <em>Текущая версия системы: R1.3 U3</em><br>
         <?php
         if (isset($_SESSION['user'])) {
             $user = unserialize($_SESSION['user']);
